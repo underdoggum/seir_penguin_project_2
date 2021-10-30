552b0e3a883a1e717468c8658606db18f7c963c6
Nathan Noack <nnoack3@gmail.com>
2021-10-30 17:52:35 -0400

    videos can now be viewed on the button

---
+++ public/app.js
@@ -0,0 +1,6 @@
+// const mongoose = require("mongoose");
+
+
+$(".button-howto").on("click", function(e) {
+  window.location.href = `https://www.youtube.com/results?search_query=how+to+${this.id}`;
+});
